5ac5d2cb3b808fef9108eeb01b8a3b1b070b6007
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-18 14:10:45 -0500

    Javascript now works as anticipated

--- password_validator/functions.js
+++ password_validator/functions.js
@@ -6,15 +6,24 @@ var showMatchError = false
 var showValidatedStatement = false
 
 var validatePasswords = function() {
+    console.log("Validating Passwords")
     let passwords = document.getElementsByName("password")
-    let unverified = passwords[0]
-    let verifier = passwords[1]
+    let unverified = passwords[0].value
+    let verifier = passwords[1].value
+    let validated = true
 
-    if(unverified !== verifier) {
-        showMatchError = true
-    } else if(unverified.length !== verifier.length) {
+    console.log(unverified.length)
+
+    if(unverified.length < 8) {
         showLengthError = true
+        validated = false
+    } else if(unverified !== verifier) {
+        showMatchError = true
+        validated = false
     } else {
         showValidatedStatement = true
     }
+
+    console.log(validated)
+    return validated
 }
