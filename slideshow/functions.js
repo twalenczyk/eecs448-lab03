b9250d0f15e98131f6fe5bf6434f09d52d55af94
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-19 21:52:18 -0500

    Changed reassignment implementation and patched photo bug

--- slideshow/functions.js
+++ slideshow/functions.js
@@ -6,12 +6,12 @@ var course = [
     "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/el-camaleon.jpg",
     "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/fieldstone-gc-1.jpg",
     "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/Kauai-Oceanfront-Golf-Course.jpg",
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/puakea_golf_course.jpg"
+    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/puakea_golf_course.jpeg"
 ]
 
 var next = function(current) {
     if(current === course[0]) {
-        document.getElementById("course").setAttribute("src", course[1])
+        document.getElementById("course").src = course[1]
     } else if(current === course[1]) {
         document.getElementById("course").src = course[2]
     } else if(current === course[2]) {
