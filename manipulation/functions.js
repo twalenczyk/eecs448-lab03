4403837c90bc00b107c55a53f034118e568cef92
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-25 00:19:02 -0400

    Form grabs values from the user

--- manipulation/functions.js
+++ manipulation/functions.js
@@ -1,2 +1,15 @@
 // Author: Taylor Walenczyk
 // Date Created: 9/24/2017
+
+var getInput = function() {
+    // border values
+    let borderRVal = document.getElementById("borderRedValue").value
+    let borderGVal = document.getElementById("borderGreenValue").value
+    let borderBVal = document.getElementById("borderBlueValue").value
+    let borderWidth = document.getElementById("borderWidth").value
+
+    // background values
+    let backgroundRVal = document.getElementById("backgroundRedValue").value
+    let backgroundGVal = document.getElementById("backgroundGreenValue").value
+    let backgroundBVal = document.getElementById("backgroundBlueValue").value
+}
