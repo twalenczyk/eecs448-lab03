c100053e0c2fa11e4a2d5a87034d6973150a7fe4
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-25 01:02:10 -0400

    User driven border impact implemented

--- manipulation/functions.js
+++ manipulation/functions.js
@@ -12,4 +12,20 @@ var getInput = function() {
     let backgroundRVal = document.getElementById("backgroundRedValue").value
     let backgroundGVal = document.getElementById("backgroundGreenValue").value
     let backgroundBVal = document.getElementById("backgroundBlueValue").value
+
+    // grab paragraph
+    let p = document.getElementById("dummyText")
+
+    // checks for color direction
+    if(borderRVal != null || borderGVal != null || borderBVal != null) {
+        let rgbValue = "rgb(" + borderRVal + ", " + borderGVal + ", " + borderBVal + ")"
+        p.style.borderColor = rgbValue
+    }
+
+    // Change border width
+    if(borderWidth != null) {
+        let bw = "" + borderWidth + "px"
+        p.style.borderWidth = bw
+        p.style.borderStyle = "solid" // forec appearance
+    }
 }
