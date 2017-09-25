df90d8f62a493d198946d52aef32fca91529c13c
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-09-25 11:38:12 -0400

    Reworded slideshow logic to alleviate file path bug

--- slideshow/functions.js
+++ slideshow/functions.js
@@ -2,41 +2,29 @@
 // Date Created: 9/18/2017
 
 var course = [
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/circling-raven-golf-course-15.jpg",
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/el-camaleon.jpg",
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/fieldstone-gc-1.jpg",
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/Kauai-Oceanfront-Golf-Course.jpg",
-    "file:///Users/twalen/Desktop/eecs_448/Lab_03/slideshow/resources/images/puakea_golf_course.jpeg"
+    "resources/images/circling-raven-golf-course-15.jpg",
+    "resources/images/el-camaleon.jpg",
+    "resources/images/fieldstone-gc-1.jpg",
+    "resources/images/Kauai-Oceanfront-Golf-Course.jpg",
+    "resources/images/puakea_golf_course.jpeg"
 ]
+var curIndex = 0
+
 
 var next = function() {
-    let current = document.getElementById('course').src
-
-    if(current === course[0]) {
-        document.getElementById("course").src = course[1]
-    } else if(current === course[1]) {
-        document.getElementById("course").src = course[2]
-    } else if(current === course[2]) {
-        document.getElementById("course").src = course[3]
-    } else if(current === course[3]) {
-        document.getElementById("course").src = course[4]
-    } else if(current === course[4]) {
-        document.getElementById("course").src = course[0]
+    let pic = document.getElementById("course")
+    curIndex++
+    if(curIndex == 5) {
+        curIndex = 0
     }
+    pic.src = course[curIndex]
 }
 
 var previous = function() {
-    let current = document.getElementById('course').src
-    
-    if(current === course[0]) {
-        document.getElementById("course").src = course[4]
-    } else if(current === course[1]) {
-        document.getElementById("course").src = course[0]
-    } else if(current === course[2]) {
-        document.getElementById("course").src = course[1]
-    } else if(current === course[3]) {
-        document.getElementById("course").src = course[2]
-    } else if(current === course[4]) {
-        document.getElementById("course").src = course[3]
+    let pic = document.getElementById("course")
+    curIndex--
+    if(curIndex == -1) {
+        curIndex = 4
     }
+    pic.src = course[curIndex]
 }
